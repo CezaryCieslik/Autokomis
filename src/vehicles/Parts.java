3f9032524c96175141ff23faef39d0c2db8162c0
CezaryCieslik <61739851+CezaryCieslik@users.noreply.github.com>
2020-06-29 18:50:40 +0200

    Added several methods in Parts

--- src/vehicles/Parts.java
+++ src/vehicles/Parts.java
@@ -21,4 +21,70 @@ public class Parts {
     public boolean isBroken(boolean isBroken){
         return isBroken;
     }
+
+    public void setBrakesBroken(){
+        this.brakes = isBroken(false);
+        System.out.println("Brakes are broken.");
+    }
+
+    public void setChassisBroken(){
+        this.chassis = isBroken(false);
+        System.out.println("Chassis is broken.");
+    }
+
+    public void setEngineBroken(){
+        this.engine = isBroken(false);
+        System.out.println("Engine is broken.");
+    }
+
+    public void setBodyBroken(){
+        this.body = isBroken(false);
+        System.out.println("Body is broken.");
+    }
+
+    public void setGearboxBroken(){
+        this.gearbox = isBroken(false);
+        System.out.println("Gearbox is broken.");
+    }
+
+    public void setBrakes(){
+        this.brakes = isBroken(true);
+    }
+
+    public void setChassis(){
+        this.chassis = isBroken(true);
+    }
+
+    public void setEngine(){
+        this.engine = isBroken(true);
+    }
+
+    public void setBody(){
+        this.body = isBroken(true);
+    }
+
+    public void setGearbox(){
+        this.gearbox = isBroken(true);
+    }
+
+    public boolean getBrakes(){
+        return this.brakes;
+    }
+
+    public boolean getChassis(){
+        return this.chassis;
+    }
+
+    public boolean getEngine(){
+        return this.engine;
+    }
+
+    public boolean getBody(){
+        return this.body;
+    }
+
+    public boolean getGearbox(){
+        return this.gearbox;
+    }
+
 }
